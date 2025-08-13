fd66a202ed58193187b570453596135a16538d4f
Honorato Rocha <honorato.junior@gmail.com>
2025-08-13 16:35:32 -0400

    Custom sampled data data

--- src/lib/ChargeStation/eventHandlers/ocpp-16/send-meter-values.ts
+++ src/lib/ChargeStation/eventHandlers/ocpp-16/send-meter-values.ts
@@ -8,33 +8,75 @@ const sendMeterValues: ChargeStationEventHandler = async ({
 }) => {
   const now = clock.now();
 
+  const meterValuesSampledData = chargepoint.configuration.getVariableValue(
+    "MeterValuesSampledData")
+
+  const sampledValue = [{
+    value: session.kwhElapsed.toFixed(5),
+    context: 'Sample.Periodic',
+    measurand: 'Energy.Active.Import.Register',
+    location: 'Outlet',
+    unit: 'kWh',
+  }];
+
+  if (meterValuesSampledData?.toString().includes("SoC")) {
+    sampledValue.push({
+      value: session.stateOfCharge.toString(),
+      context: 'Sample.Periodic',
+      measurand: 'SoC',
+      location: 'EV',
+      unit: 'Percent',
+    });
+  }
+
+  if (meterValuesSampledData?.toString().includes("Power.Active.Import")) {
+    sampledValue.push({
+      value: '7.0',
+      context: 'Sample.Periodic',
+      measurand: 'Power.Active.Import',
+      location: 'Outlet',
+      unit: 'kW',
+    });
+  }
+
+  if (meterValuesSampledData?.toString().includes("Current.Import")) {
+    sampledValue.push({
+      value: '32.0',
+      context: 'Sample.Periodic',
+      measurand: 'Current.Import',
+      location: 'Outlet',
+      unit: 'A',
+    });
+  }
+
+  if (meterValuesSampledData?.toString().includes("Voltage")) {
+    sampledValue.push({
+      value: '230',
+      context: 'Sample.Periodic',
+      measurand: 'Voltage',
+      location: 'Outlet',
+      unit: 'V',
+    });
+  }
+
+  if (meterValuesSampledData?.toString().includes("Temperature")) {
+    sampledValue.push({
+      value: '36.0',
+      context: 'Sample.Periodic',
+      measurand: 'Temperature',
+      location: 'Outlet',
+      unit: 'Celcius',
+    });
+  }
+
   chargepoint.writeCall<MeterValuesRequest>('MeterValues', {
     connectorId: session.connectorId,
     transactionId: Number(session.transactionId),
     meterValue: [
       {
         timestamp: now.toISOString(),
-        sampledValue: [
-          {
-            value: session.kwhElapsed.toFixed(5),
-            context: 'Sample.Periodic',
-            measurand: 'Energy.Active.Import.Register',
-            location: 'Outlet',
-            unit: 'kWh',
-          },
-        ],
-      },
-      {
-        timestamp: now.toISOString(),
-        sampledValue: [
-          {
-            value: session.stateOfCharge.toString(),
-            context: 'Sample.Periodic',
-            measurand: 'SoC',
-            location: 'EV',
-            unit: 'Percent',
-          },
-        ],
+        // @ts-ignore
+        sampledValue: sampledValue,
       },
     ],
   });
